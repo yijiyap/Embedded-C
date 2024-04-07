7f7f758bbc42d8477040af95bf737cb1f4ef7a16
yijiyap <yijiyap@gmail.com>
2024-04-07 12:26:55 +0800

    workspace setup in STM IDE

---
+++ My_workspace/host/000Test/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 7, 2024
+ *      Author: yj
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello world\n");
+
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+}
